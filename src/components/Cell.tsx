f8931e36544bc62fed66fe5d6f7d9f59961f8bd7
Vishal <vishalkhanna995@gmail.com>
2025-02-09 21:35:13 +0100

    minor refactoring

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -4,11 +4,7 @@ import { CellValue, UpdateBoardState } from "../util/types";
 
 export default function Cell({ cellValue, updateBoardState, isGameInProgress }: {cellValue: CellValue, updateBoardState: UpdateBoardState, isGameInProgress: boolean}) {
     const isDisabled = cellValue.symbol != EMPTY_SYMBOL || !isGameInProgress;
-
-    let className = "w-20 h-20 disabled:cursor-not-allowed";
-    if (cellValue.isWinCell) {
-        className += " text-red-400"
-    }
+    const className = `w-20 h-20 disabled:cursor-not-allowed ${cellValue.isWinCell ? 'text-red-40"' : ''}`;
 
     return (
         <button className={className} onClick={() => updateBoardState(cellValue.id)} disabled={isDisabled}>{cellValue.symbol}</button>
